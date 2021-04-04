2b99d9755c4ba58e1ebbf61048a316151f85580d
luis <luis.sg.vitor@gmail.com>
2021-04-04 20:49:16 -0300

    Creating routes file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => console.log('server is running'));
