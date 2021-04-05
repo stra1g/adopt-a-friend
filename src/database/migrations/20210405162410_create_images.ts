8b71164602b0080dda9f7b4f938b6d58a0dc2ede
luis <luis.sg.vitor@gmail.com>
2021-04-05 13:30:51 -0300

    Creating images table by migration

---
+++ src/database/migrations/20210405162410_create_images.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('images', table => {
+    table.uuid('id').notNullable().unique().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.text('path').notNullable()
+    table.uuid('user_id').references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE').nullable()
+    table.uuid('animal_id').references('id').inTable('animals').onUpdate('CASCADE').onDelete('CASCADE').nullable()
+
+    table.timestamp('created_at').defaultTo((new Date()).toUTCString())
+    table.timestamp('updated_at').defaultTo(new Date().toUTCString())
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('images');
+}
+
