3e5ca74d3bb97e0a823b86cf0fa3c174d0c9a2b0
luis <luis.sg.vitor@gmail.com>
2021-04-05 15:40:05 -0300

    Separating server to app

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,3 @@
-import express from 'express';
-import routes from './routes';
-import path from 'path'
-
-const app = express();
-
-app.use(routes);
-
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+import app from './app'
 
 app.listen(process.env.PORT || 3333, () => console.log('server is running'));
